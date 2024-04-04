13bb51d15b1951606368d27970fb104f71da6a67
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2024-04-04 09:36:31 +0530

    Add HomeComponent and update Food model

--- frontend/src/app/shared/models/Food.ts
+++ frontend/src/app/shared/models/Food.ts
@@ -1,11 +1,11 @@
-export class Food{
-    id!:string;   //! for required field
-    name!:string;
-    price!:number;
-    tags?: string[];   // optional field - ?
-    favorite!:boolean;
-    stars!: number;
-    imageUrl!: string;
-    origins!: string[];
-    cookTime!:string;
-  }
+export class Food {
+  id!: string; //! for required field
+  name!: string;
+  price!: number;
+  tags?: string[]; // optional field - ?
+  favorite!: boolean;
+  stars!: number;
+  imageUrl!: string;
+  origins!: string[];
+  cookTime!: string;
+}
