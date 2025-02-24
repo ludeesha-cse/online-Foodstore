9816ce0ea1d9563d1615ec22560dd2a802974afa
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2025-02-25 04:00:57 +0530

    Refactor authentication controller to use service methods for login and registration; improve error handling and token management

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model";
 
-const generateTokenResponse = (user: User, req: any, res: any) => {
+export const generateTokenResponse = (user: User): string => {
   const token = jwt.sign(
     {
       id: user.id,
@@ -11,16 +11,5 @@ const generateTokenResponse = (user: User, req: any, res: any) => {
     process.env.JWT_SECRET!, // Replace with a valid secret or private key
     { expiresIn: "1d" }
   );
-  user.token = token;
-
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: true,
-    maxAge: 24 * 60 * 60 * 1000,
-  });
-
-  //console.log(token)
-  return user;
+  return token;
 };
-
-export default generateTokenResponse;
